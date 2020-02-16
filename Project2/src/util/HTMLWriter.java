41c8097dae9669012a86cfe8355dfc292ca9b159
Matt Padgett <matthew.padgett@ttu.edu>
2020-02-15 22:14:57 -0600

    Added HTMLWriter class and Main class

---
+++ Project2/src/util/HTMLWriter.java
@@ -0,0 +1,21 @@
+/* CS 2365 Object-Oriented Programming - Spring 2020
+ * Project 2 - UNO Exercises
+ * File: HTMLWriter.java
+ * Date: 02/15/2020
+ * Author: Matthew Padgett - matpadge - R11564420
+ * Contributors: None.
+ *
+ * Description: A grouping of static methods to append to an output HTML file. This does not include styling.
+ */
+
+package util;
+
+public class HTMLWriter {
+    
+    public static void appendHand(Hand hand) {
+        
+        
+        
+    }
+    
+}
