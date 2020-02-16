12e533a1f8ecf73910f24939150bbf908f6b53bc
noitschristian <christian.pennington@ttu.edu>
2020-02-15 22:14:56 -0600

    Added Card class

---
+++ Project2/src/model/Card.java
@@ -0,0 +1,18 @@
+/* CS 2365 Object-Orientated Programming - Spring 2020
+ * Project 2 - Uno Exercise
+ * File: Card.java
+ * Date: 02/12/2020
+ * Author: Christian Pennington
+ * Contributors: None
+ * 
+ * Description: a collection of static methods to create the varying cards used in the project
+ * 
+ */
+
+package model;
+
+import java.awt.Color;
+
+public class Card {
+
+}
