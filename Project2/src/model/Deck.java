d649f73cff5f52af187680b6c9d4cd91752cd2fd
JasonLuckow <jason.luckow@ttu.edu>
2020-02-24 21:57:02 -0600

    codigo

--- Project2/src/model/Deck.java
+++ Project2/src/model/Deck.java
@@ -12,79 +12,91 @@ package model;
 import java.awt.Color;
 
 public class Deck {
-    int count, topIndex, num, connected, deckSize;
+    int count, topIndex, num, deckSize;
+    boolean connected, includeActionCards;
     private Card[] cards;
     
     public Deck (){//CONSTRUCTOR
         
         this.count = 0;
         this.topIndex = 0;  
-        this.deckSize = 180;
-        this.cards = new Card();
+        this.deckSize = 108;
+        this.cards = new Card[324];
+        this.includeActionCards = true;
         
     }
     
-    public Deck(int num, boolean connected) {
+    public Deck (int numDecks, boolean connected, boolean includeActionCards){
         
-        deckSize *= num;
+        deckSize *= numDecks;
+        Color c = Color.WHITE;
         int arrayCounter = 0;
         
-        while (arrayCounter < deckSize){
-            
-            for (int j = 0; j < 4; j++){
+        for (int m = 0; m <= numDecks; m++){
+
+            for (int j = 0; j <= 4; j++){ //LOOP FOR EACH COLOR
+
+                switch (j){
+
+                    case 0:
+                        c = Color.BLUE;
+                        break;
+                    case 1:
+                        c = Color.GREEN;
+                        break;
+                    case 2:
+                        c = Color.YELLOW;
+                        break;
+                    case 4:
+                        c = Color.RED;
+                        break;  
+                        
+                }
+
+                cards[arrayCounter] = new Card (c, 0);//ZERO ONLY HAS ONE RANK
+                arrayCounter++;
                 
-                for (int k = 0; k < 2; k++){
+                if (includeActionCards == true){
                     
-                    for (int i = 0; i < 12; i++) {
-                        
-                        Color c = Color.WHITE;
-                        switch (j){
-                            case 0:
-                                c = Color.BLUE;
-                                break;
-                            case 1:
-                                c = Color.GREEN;
-                                break;
-                            case 2:
-                                c = Color.YELLOW;
-                                break;
-                            case 4:
-                                c = Color.RED;
-                                break;
-                        }
-                        
-                        cards[deckSize] = new Card(c,i);
-                        
-                    }
+                    cards[arrayCounter] = new Card (Color.BLACK, 13);//WILD
+                    arrayCounter++;
+                    cards[arrayCounter] = new Card (Color.BLACK, 14);//WILD DRAW FOUR
+                    arrayCounter++;
                     
                 }
-                
-            }
-            
+
+                for (int k = 0; k <= 2; k++){//EACH COLOR HAS TWO RANKS
+
+                    for (int i = 1; i <= 12; i++){//RANK LOOP
+
+                        cards[arrayCounter] = new Card(c,i);
+                        arrayCounter++;
+                        
+                    }    
+                }    
+            }   
         }
-        
-        
     }
     
     //GETTERS
-    public int getCount() {
+    public int getCount(){
         
         return count;
         
     }
-    public int getTopIndex () {
+    public int getTopIndex (){
         
         return topIndex;
         
     }
     
     //SETTERS
-    public void setCount(int count) {
+    public void setCount(int count){
         
         this.count = count;
         
     }
-    public void setTopIndex(int topIndex) {
+    public void setTopIndex(int topIndex){
         
         this.topIndex = topIndex;
         
